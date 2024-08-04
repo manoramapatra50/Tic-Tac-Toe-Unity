87959bcced2208cc71ebe3fe9cee61abeaf729bd
manoramapatra50 <tatmanorma@gmail.com>
2024-08-04 21:26:44 +0530

    Added Game logic

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class BoardManager : NetworkBehaviour
 {
     Button[,] allBtn = new Button[3, 3];
+    int[,] allBtnValue = new int[3, 3];
     [SerializeField] private Sprite xSprite;
     [SerializeField] private Sprite oSprite;
     public override void OnNetworkSpawn()
@@ -18,6 +19,7 @@ public class BoardManager : NetworkBehaviour
             for (int j = 0; j < 3; j++)
             {
                 allBtn[i, j] = btns[k];
+                allBtnValue[i, j] = -1;
                 k++;
 
                 int row = i;
@@ -33,18 +35,21 @@ public class BoardManager : NetworkBehaviour
 
     void OnClickBtn(int row, int col)
     {
-        if(NetworkManager.Singleton.IsHost)
+        if(NetworkManager.Singleton.IsHost && GameManager.inst.currentTurn.Value == 0)
         {
+            allBtnValue[row, col] = 1;
             allBtn[row, col].GetComponent<Image>().sprite = xSprite;
             ChangeSpriteOnClientRPC(row, col);
-
+            GameManager.inst.currentTurn.Value = 1;
+            CheckResult(row, col);
             Debug.Log("is host");
         }
-        else if (NetworkManager.Singleton.IsClient)
+        else if (!NetworkManager.Singleton.IsHost && GameManager.inst.currentTurn.Value == 1)
         {
+            allBtnValue[row, col] = 0;
             allBtn[row, col].GetComponent<Image>().sprite = oSprite;
             ChangeSpriteOnServerRPC(row, col);
-
+            CheckResult(row, col);
             Debug.Log("is client");
         }
     }
@@ -52,6 +57,7 @@ public class BoardManager : NetworkBehaviour
     [ClientRpc]
     void ChangeSpriteOnClientRPC(int row,int col)
     {
+        allBtnValue[row, col] = 1;
         allBtn[row, col].GetComponent<Image>().sprite = xSprite;
         allBtn[row, col].interactable = false;
 
@@ -60,9 +66,57 @@ public class BoardManager : NetworkBehaviour
     [ServerRpc(RequireOwnership = false)]
     void ChangeSpriteOnServerRPC(int row, int col)
     {
+        allBtnValue[row, col] = 0;
         allBtn[row, col].GetComponent<Image>().sprite = oSprite;
         allBtn[row, col].interactable = false;
 
+        GameManager.inst.currentTurn.Value = 0;
+
         Debug.Log("is host server");
+    }
+
+    void CheckResult(int row, int col)
+    {
+       
+        if(IsWon(row, col))
+        {
+            GameManager.inst.ShowMsg("won");
+        }
+        else
+        {
+            if (IsGameDraw())
+            {
+                GameManager.inst.ShowMsg("draw");
+            }
+        }
+    }
+
+    public bool IsWon(int row, int col)
+    {
+        int clickedBtnValue = allBtnValue[row, col];
+        // check for all column
+        if (allBtnValue[0, col] == clickedBtnValue && allBtnValue[1, col] == clickedBtnValue && allBtnValue[2, col] == clickedBtnValue) return true;
+
+        // check for all row
+        if (allBtnValue[row, 0] == clickedBtnValue && allBtnValue[row, 1] == clickedBtnValue && allBtnValue[row, 2] == clickedBtnValue) return true;
+
+        // check for diagonals
+        if (allBtnValue[0, 0] == clickedBtnValue && allBtnValue[1, 1] == clickedBtnValue && allBtnValue[2, 2] == clickedBtnValue) return true;
+
+        if (allBtnValue[0, 2] == clickedBtnValue && allBtnValue[1, 1] == clickedBtnValue && allBtnValue[2, 0] == clickedBtnValue) return true;
+
+        return false;
+    }
+
+    bool IsGameDraw()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (allBtnValue[i, j] == -1) return false;
+            }
+        }
+        return true;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : NetworkBehaviour
 {
     public static GameManager inst;
+    public NetworkVariable<int> currentTurn = new NetworkVariable<int>(0);
+
     [SerializeField]private GameObject boardPrefab;
+    [SerializeField] private GameObject resultPanel;
+    [SerializeField] private TextMeshProUGUI msgText;
+
 
     private void Awake()
     {
@@ -42,5 +48,47 @@ public class GameManager : MonoBehaviour
         GameObject board = Instantiate(boardPrefab);
         var instanceNetworkObject = board.GetComponent<NetworkObject>();
         instanceNetworkObject.Spawn();
+    }
+
+    public void ShowMsg(string msg)
+    {
+        if(msg.Equals("won"))
+        {
+            msgText.text = "You Won";
+            resultPanel.SetActive(true);
+            ShowOpponentMsg("You Loose");
+        }
+        else if(msg.Equals("draw"))
+        {
+            msgText.text = "Game Draw";
+            resultPanel.SetActive(true);
+            ShowOpponentMsg("Game Draw");
+        }
+    }
+
+    void ShowOpponentMsg(string msg)
+    {
+        if(IsHost)
+        {
+            ShowOpponentMsgClientRpc(msg);
+        }
+        else 
+        {
+            ShowOpponentMsgServerRpc(msg);
+        }
+    }
+    [ClientRpc]
+    void ShowOpponentMsgClientRpc(string msg)
+    {
+        if (IsHost) return;
+        
+        msgText.text = msg;
+        resultPanel.SetActive(true);
+    }
+    [ServerRpc(RequireOwnership =false)]
+    void ShowOpponentMsgServerRpc(string msg)
+    {
+        msgText.text = msg;
+        resultPanel.SetActive(true);
     }
 }
